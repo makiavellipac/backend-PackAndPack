6f600ba46177d64c42934db9968c6f6f67998e2b
MakiavelliPac <ridesburak@gmail.com>
2020-05-11 17:44:31 -0500

    Se creo la logica del servidor y se conecto la base de datos

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error('Error connecting to mongo', err));
 
 const app=express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`listen on http://localhost:${process.env.PORT}`)
