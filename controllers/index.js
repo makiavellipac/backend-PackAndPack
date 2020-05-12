6a8a77bbb5cf02475903340b750b4a571da0e065
MakiavelliPac <ridesburak@gmail.com>
2020-05-12 12:41:50 -0500

    Se terminaron los endPoint y se probaron Nota:Checar funcion getDateForConsulting si es que hay futuros problemas

---
+++ controllers/index.js
@@ -0,0 +1,54 @@
+const user=require("../models/user");
+
+const getDateForConsulting=()=>{
+    fecha=new Date();
+    fecha.setDate(fecha.getDate()-2);
+    fecha.setHours(-5,00,00);
+    return fecha
+  }
+
+exports.newRegistry=async(req,res)=>{
+    const {name,phone,age,gender}=req.body;
+    await user.create({name,phone,age,gender})
+    .then((users)=>res.status(201).json({users}))
+    .catch((err)=>res.status(400).json({err}))
+}
+
+exports.consultUser=async(req,res)=>{
+    const {userName}=req.params
+    if(userName){
+        const data=await user.find({name:userName})
+        res.status(200).json({data})
+    }
+    else{
+        const data=await user.find()
+        res.status(200).json({data})
+    }
+}
+
+exports.deleteRegistry=async(req,res)=>{
+    const {_id}=req.params
+    await user.findByIdAndDelete({_id})
+    .then((users)=>res.status(201).json({users}))
+    .catch((err)=>res.status(400).json({err}))
+}
+
+exports.consultByAge=async(req,res)=>{
+    //console.log(getDateForConsulting())
+    const data=await user.find(
+        {$and:[{
+            $and:[
+                {age:{
+                    $gt:18
+                }},
+                {gender:"Male"}
+            ]
+        },
+        {createdAt:{
+            $gt:getDateForConsulting()
+        }}
+        ]}
+    )
+    res.status(200).json({data})
+}
+//{$and:[{$and:[{age:{$gt:18}},{gender:"Male"}]},{createdAt:{$gt:new ISODate("2020-05-12T04:50:30Z")}}]}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const {Router}=require('express')
 
+const {
+    newRegistry,
+    consultUser,
+    deleteRegistry,
+    consultByAge
+}=require('../controllers/index')
+
 const router=Router();
 
-router.get('/',(req,res)=>res.status(200).send(`Api Ready`))
+router
+.get('/',(req,res)=>res.status(200).send(`Api Ready`))
+.post('/newRegistry',newRegistry)
+.get('/consultUser/:userName',consultUser)
+.get('/consultUser',consultUser)
+.delete('/deleteRegistry/:_id',deleteRegistry)
+.get('/consultByAge',consultByAge)
 
 module.exports=router;
